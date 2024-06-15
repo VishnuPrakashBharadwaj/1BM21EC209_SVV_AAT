c77b55f8c48b12ea7f15d4bb7606866525fce9f6
Vishnu Prakash Bharadwaj <39427770+VishnuPrakashBharadwaj@users.noreply.github.com>
2024-06-15 10:50:24 +0530

    interface update

--- problem_1/TEST/sorter_test.sv
+++ problem_1/TEST/sorter_test.sv
@@ -2,15 +2,14 @@
 
 program sorter_test(sorter_if.TEST_MP sortif);
 	driver drv_dut;
-
+	
 	initial begin
 		drv_dut = new(sortif);
 		repeat (10) begin 
 			drv_dut.drive_ips();
 		end
-		
-		$finish;
-   end
 
+		$finish;
+	end
 endprogram
 
--- problem_2/TEST/even_ones_test.sv
+++ problem_2/TEST/even_ones_test.sv
@@ -3,13 +3,11 @@
 program even_ones_test(even_ones_if.TEST_MP sortif);
     driver drv_dut;
 
-	initial begin
-	    drv_dut = new(sortif);
-
+    initial begin
+        drv_dut = new(sortif);
 		repeat (20) begin 
-			drv_dut.drive_ips();
-		end
-  
+            drv_dut.drive_ips();
+        end
 		$finish;
-   end
+    end
 endprogram
