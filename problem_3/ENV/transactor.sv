a3e517985cff96a98230a6b8f1f78332907ba2e5
Vishnu Prakash Bharadwaj <39427770+VishnuPrakashBharadwaj@users.noreply.github.com>
2024-06-15 16:04:22 +0530

    Update transactor.sv

--- problem_3/ENV/transactor.sv
+++ problem_3/ENV/transactor.sv
@@ -1,5 +1,20 @@
 class transactor;
+    rand bit en, M;
+    rand bit rst;
+
+    constraint in_constraint {
+        //Set the probability of input being 0.5
+        in dist { [0:0] := 1, [1:1] := 1 };
+    }
+
     function new();
-        
+        $$display("Transactor for counter initiated.");
     endfunction //new()
+
+    task disp;
+        begin
+            $display("**********NEW TRANSACTION***************");
+            $display("FROM TRANSACTOR: INPUT GENERATED = %b and RESET = %b", in, rst);
+        end
+    endtask
 endclass //transactor
